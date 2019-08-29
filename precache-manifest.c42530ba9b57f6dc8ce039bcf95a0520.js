e1508f1ac45259d3a7c182c124d0490543e8044e
Jose Mendoza <joseemendoza194@gmail.com>
2019-08-29 01:38:21 -0700

    Updates

---
+++ precache-manifest.c42530ba9b57f6dc8ce039bcf95a0520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1a66a16b6c407ec14a3bd8b4e02f30",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "566a1e907733e1d05a38",
+    "url": "/monsters-rolodex/static/css/main.2f52394c.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monsters-rolodex/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "566a1e907733e1d05a38",
+    "url": "/monsters-rolodex/static/js/main.c4a5b744.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
